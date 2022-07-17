0ad1e98b2134bdc5b3a9edea5943c25496db90ee
Germen-bit <jgermen14@gmail.com>
2022-07-17 14:01:24 +0200

    Route for create a post, return all the posts and find a post by ID created

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,59 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const passport = require('passport')
+
+// Load post model
+const Post = require('../../models/Post')
+
+// Load post validation
+const validatePostInput = require('../../validation/post')
 
 // @route   GET api/posts/test
 // @desc    Tests posts route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: "Posts Works" }))
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validatePostInput(req.body)
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
+
+  const newPost = new Post ({
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    user: req.user.id,
+  })
+
+  // Save post
+  newPost.save().then(post => res.json(post))
+})
+
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json(err))
+})
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .sort({ date: -1 })
+    .then(profile => res.json(profile))
+    .catch(err => res.status(404).json(err))
+})
 
 module.exports = router
