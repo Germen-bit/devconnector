782506ba52f96754f01e9e421bf75307a70352d1
Germen-bit <jgermen14@gmail.com>
2022-07-17 15:02:34 +0200

    Delete post route created

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,9 @@ const passport = require('passport')
 
 // Load post model
 const Post = require('../../models/Post')
+// Load Profile model
+const Profile = require('../../models/Profile')
+
 
 // Load post validation
 const validatePostInput = require('../../validation/post')
@@ -50,10 +53,39 @@ router.get('/', (req, res) => {
 // @desc    Get post by id
 // @access  Public
 router.get('/:id', (req, res) => {
+  const errors = {}
+
   Post.findById(req.params.id)
     .sort({ date: -1 })
     .then(profile => res.json(profile))
-    .catch(err => res.status(404).json(err))
+    .catch(err => {
+      errors.postnotfound = 'Post does not exist'
+      res.status(404).json(errors)
+    })
 })
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post by id
+// @access  Private
+router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // Check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ notauthorized: 'User no authorized'})
+          }
+
+          // Delete post
+          post.remove().then(() => res.json({ success: true}))
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'Post not found' }))
+    })
+    .catch(err => res.status(400).json(err))
+})
+
+
 
 module.exports = router
